cb5d086a39b8a5a14fe393eb028afb0b7d4121fd
Mijail N <89676694+mijail-naal@users.noreply.github.com>
2024-08-08 17:36:41 -0600

    Create jwt_settings file for AuthJWT

--- auth_service/auth/src/schemas/user.py
+++ auth_service/auth/src/schemas/user.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from datetime import timedelta, datetime
+from datetime import datetime
 
 from pydantic import BaseModel, EmailStr, Field
 
@@ -51,16 +51,6 @@ class UserHistoryInDB(BaseModel):
     id: UUID
     user_id: UUID
     logged_at: datetime
-
-
-class JTWSettings(BaseModel):
-    authjwt_secret_key: str = "secret"
-    authjwt_denylist_enabled: bool = True
-    authjwt_denylist_token_checks: set = {"access", "refresh"}
-    authjwt_token_location: set = {"cookies"}
-    authjwt_cookie_csrf_protect: bool = False
-    access_expires: int = timedelta(minutes=15)
-    refresh_expires: int = timedelta(days=30)
 
 
 class UserAccess(BaseModel):
